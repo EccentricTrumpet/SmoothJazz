b3b7651401fad3832ffe5c08aadaab49706ec947
tabletenniser <tabletenniser@gmail.com>
2021-05-08 00:49:12 -0700

    Clean up repository

--- local_test.sh
+++ local_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 start() {
-  python3 ./grpc_server/server.py > backend.log 2>&1 &
-  grpcwebproxy  --backend_addr=localhost:50051 --backend_tls=False --server_tls_cert_file=./grpc_server/misc/localhost.crt --server_tls_key_file=./grpc_server/misc/localhost.key --allow_all_origins --server_http_max_write_timeout=1h --server_http_max_read_timeout=1h > proxy.log 2>&1 &
+  python3 ./backend/server.py > backend.log 2>&1 &
+  grpcwebproxy  --backend_addr=localhost:50051 --backend_tls=False --server_tls_cert_file=./backend/misc/localhost.crt --server_tls_key_file=./backend/misc/localhost.key --allow_all_origins --server_http_max_write_timeout=1h --server_http_max_read_timeout=1h > proxy.log 2>&1 &
   cd ./frontend && ionic serve > frontend.log 2>&1 &
 }
 
