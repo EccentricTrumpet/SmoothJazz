8e9d484c0e0d2ced238b130434e76d73cf278b8b
= <johluo@microsoft.com>
2021-06-13 03:01:10 -0700

    Restore CI

--- regenerate_protos.sh
+++ regenerate_protos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-protoc --plugin=protoc-gen-ts=./frontend/node_modules/.bin/protoc-gen-ts --js_out=import_style=commonjs,binary:frontend/proto-gen --ts_out=service=grpc-web:frontend/proto-gen -I ./backend/protos/ ./backend/protos/shengji.proto
+protoc --js_out=import_style=commonjs,binary:proto-gen --grpc-web_out=import_style=typescript,mode=grpcwebtext:proto-gen -I ./backend/protos/ ./backend/protos/shengji.proto
 cd backend
 pipenv run python -m grpc_tools.protoc -I ./protos --python_out=. --grpc_python_out=. ./protos/shengji.proto
