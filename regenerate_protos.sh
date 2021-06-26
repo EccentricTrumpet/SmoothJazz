618f1cd1a7fa284d9fe46d4292660077975e8c2e
John Luo <johluo@microsoft.com>
2021-06-26 11:54:09 -0700

    Print friendly string for cardProto (#83)
    Co-authored-by: = <jtluo@mail.utoronto.ca>

--- regenerate_protos.sh
+++ regenerate_protos.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-protoc --js_out=import_style=commonjs,binary:proto-gen --grpc-web_out=import_style=typescript,mode=grpcwebtext:proto-gen -I ./backend/protos/ ./backend/protos/shengji.proto
-cd backend
-pipenv run python -m grpc_tools.protoc -I ./protos --python_out=. --grpc_python_out=. ./protos/shengji.proto
+(cd frontend; npm run prebuild)
+(cd backend; pipenv run python -m grpc_tools.protoc -I ./protos --python_out=. --grpc_python_out=. ./protos/shengji.proto)
