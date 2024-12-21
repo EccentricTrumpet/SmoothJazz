24351fb4d1f019e49be708f117e459b97e7c6886
JunTaoLuo <jtluo@alumni.cmu.edu>
2024-12-20 20:34:44 -0500

    Remove legacy code

--- legacy/frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  grpcUrl: "http://smoothjazz.ddns.net:8080"
-};
--- legacy/regenerate_protos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-(cd frontend; npm run prebuild)
-(cd backend; pipenv run python -m grpc_tools.protoc -I ./protos --python_out=. --grpc_python_out=. ./protos/shengji.proto)
